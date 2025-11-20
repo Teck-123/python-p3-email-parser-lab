7151ec2d6f1f55e69035a079ec7a8024b9f84691
Thecla Owano <teckowano@gmail.com>
2025-11-20 15:08:11 +0300

    email

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,13 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, string):
+        self.string = string
+
+    def parse(self):
+        # Regex pattern to match email addresses
+        email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+        # Find all matches
+        emails = re.findall(email_pattern, self.string)
+        # Return in reverse order
+        return emails[::-1]
